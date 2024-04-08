9500f0615922ea04fcf3111a6ab58400600ff0c5
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-04-08 16:40:37 +0200

    [Working] My Milestone 4 -> Add some style on my Header + Add all(?) Jumbotron images

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,71 +3,71 @@ export default {
     name: "AppHeader",
     data() {
         return {
-
+            menuOptions: ["HOMES", "PAGES", "BLOG", "SHOP", "EVENTS", "ELEMENTS",],
+            menuIcons: ['fa-solid fa-magnifying-glass', 'fa-solid fa-bars']
         }
     },
 }
 </script>
 
 <template>
-    <header>
+    <header class=" position-realative">
         <!--NavBar-->
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <nav class="navbar navbar-expand-lg position-absolute z-1 w-100 pt-5 text-dark fw-bold">
             <div class="container-fluid">
-                <a class="navbar-brand " href="#">Navbar</a>
+                <!--Qui il Logo-->
+                <a class="navbar-brand" href="#">
+                    <img src="../assets/img/mylogo/logo-img-01.png" alt="" class="mylogo">
+                </a>
+
+                <!-- Inserisco dinamicamente il menu-->
                 <div class="collapse navbar-collapse flex-grow-0 ml-auto" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <li v-for="option in menuOptions" class="nav-item">
+                            <a class="nav-link" href="#">{{ option }}</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
+                        <li v-for="icon in menuIcons">
+                            <a class="nav-link" href="#"> <i :class="icon"></i></a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Link</a>
-                        </li>
-
                     </ul>
 
                 </div>
             </div>
         </nav>
+        <!--/NavBar-->
 
         <!--Jumbo/Carosuel-->
-        <div id="carouselExampleDark" class="carousel carousel-dark slide">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active"
-                    aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1"
-                    aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2"
-                    aria-label="Slide 3"></button>
-            </div>
+        <div id="carouselExample" class="carousel slide">
             <div class="carousel-inner">
-                <div class="carousel-item active" data-bs-interval="10000">
-                    <img src="../assets/img/01.jpg" class="d-block w-100">
-                    <div class="carousel-caption d-none d-md-block">
-                        <h5>First slide label</h5>
-                        <p>Some representative placeholder content for the first slide.</p>
+                <div class="carousel-item active">>
+                    <img src="../assets/img/jumbo-slider/rev-slider-main-home-img-01.jpg" class="d-block w-100"
+                        alt="...">
+                    <div class="carousel-caption text-dark text-start">
+                        <h1 class="">Our Team</h1>
+                        <p class="fs-6 text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione eveniet
+                            dicta eaque quos atque veritatis saepe similique sequi ipsam mollitia?</p>
+                        <button type="" class="my_btn bg_myprimary">READ MORE | -></button>
+                        <button type="" class="my_btn bg_mylight">PURCHASE | -></button>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="../assets/img/jumbo-slider/rev-slider-main-home-img-02.png" class="d-block w-100"
+                        alt="...">
+                    <div class="carousel-caption text-dark text-start">
+                        <h1 class="">Our Team</h1>
+                        <p class="fs-6 text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione eveniet
+                            dicta eaque quos atque veritatis saepe similique sequi ipsam mollitia?</p>
+                        <button type="" class="my_btn bg_myprimary">READ MORE | -></button>
+                        <button type="" class="my_btn bg_mylight">PURCHASE | -></button>
                     </div>
                 </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark"
-                data-bs-slide="prev">
+
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark"
-                data-bs-slide="next">
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
@@ -75,4 +75,17 @@ export default {
     </header>
 </template>
 
-<style scoped></style>
+<style>
+header {
+    margin-top: -2rem;
+
+    .mylogo {
+        max-width: 100px;
+    }
+}
+
+.carousel-caption {
+    width: 35%;
+    top: 35%
+}
+</style>
