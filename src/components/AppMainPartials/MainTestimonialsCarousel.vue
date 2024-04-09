11573643e513d8bb14b9a9d48ece0488f32296b0
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-04-09 17:35:46 +0200

    [Working] My Milestone 11 -> Add content on my compoents

--- src/components/AppMainPartials/MainTestimonialsCarousel.vue
+++ src/components/AppMainPartials/MainTestimonialsCarousel.vue
@@ -3,7 +3,7 @@ export default {
     name: "MainTestimonialsCarousel",
     data() {
         return {
-
+            myTestimonials: ["07", "08"]
         }
     },
 }
@@ -15,25 +15,32 @@ export default {
 
         <!--carousel-->
         <div id="carouselFive" class="carousel position-relative carousel-dark slide w-100">
-            <h1 class="text-center position-absolute w-100">Testimonials</h1>
+            <h1 class="text-center w-100 position-absolute">Testimonials.</h1>
             <div class="carousel-inner testimonials_item d-flex">
+                <div class="carousel-indicators ">
+                    <button type="button" data-bs-target="#carouselFive" data-bs-slide-to="0" class="active"
+                        aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselFive" data-bs-slide-to="1"
+                        aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselFive" data-bs-slide-to="2"
+                        aria-label="Slide 3"></button>
+                </div>
                 <div class="carousel-item active position-relative d-flex justify-content-center"
                     data-bs-interval="10000">
-                    <img src="/public/img/events/h1-img-01.jpg" class="h-25 align-self-center">
-                    <div class="carousel-caption bottom-0">
-                        <h5>First slide label</h5>
+                    <img src="/public/img/testimonials/h3-img-04.png" class="h-25 align-self-center">
+                    <div class="carousel-caption bottom-0 text-light">
+                        <h6>Cinzia Clark</h6>
                         <p>Some representative placeholder content for the first slide.</p>
                     </div>
+
                 </div>
             </div>
 
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark"
-                data-bs-slide="prev">
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselFive" data-bs-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Previous</span>
             </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark"
-                data-bs-slide="next">
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselFive" data-bs-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="visually-hidden">Next</span>
             </button>
@@ -41,9 +48,14 @@ export default {
 
         <!--Video-->
 
-        <img src="/public/img/events/h1-img-01.jpg" class="d-block w-100">
+        <img src="/public/img/events/h1-img-04.jpg" class="d-block w-100">
 
     </div>
 </template>
 
-<style></style>
+<style>
+#carouselFive h1 {
+    font-size: 150px;
+
+}
+</style>
