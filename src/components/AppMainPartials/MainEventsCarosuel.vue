9feea7fd193a8ecac5ba439621e179b1a5967afc
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-04-10 11:55:07 +0200

    [Completed] Style on OurSpecialties and EventsCarosuel

--- src/components/AppMainPartials/MainEventsCarosuel.vue
+++ src/components/AppMainPartials/MainEventsCarosuel.vue
@@ -21,56 +21,28 @@ export default {
             <div class="carousel-item" v-for="img in eventImgs">
                 <img :src="'/public/img/events/h1-img-' + img + '.jpg'" class="d-block w-100" alt="...">
             </div>
-            <div class="event_tab w-75 carousel-caption text-dark text-start">
+            <div class="event_tab w-50 carousel-caption text-dark text-start">
                 <div class="wrapper col-6 bg-light">
-                    <h3 class="p-2">Upcoming Events</h3>
+                    <h3 class="p-3 ps-4 fw-bold">Upcoming Events</h3>
                     <div class="row flex-column">
-                        <div class="col ">
-                            <div class="events-card border d-flex">
-                                <div class="col-3 d-flex justify-content-center pt-3">
-                                    <div class="square d-flex flex-column align-items-center justify-content-center">
-                                        <div class="">07</div>
+                        <div class="col" v-for="index in 3">
+                            <div class="left_event events-card border d-flex p-4 pb-2">
+                                <div class="col-3 d-flex justify-content-center ">
+                                    <div
+                                        class="text-light  bg_myprimary square d-flex flex-column align-items-center justify-content-center">
+                                        <div class="fw-bold">07</div>
                                         <span>June 2022</span>
                                     </div>
                                 </div>
-                                <div class="col-9 d-flex flex-column pt-3 align-items-start">
-                                    <h4>New York Coaching</h4>
-                                    <p><i class="fa-regular fa-clock"></i> 9:00 am - 5 p.m, Jan 7, 2022</p>
-                                    <p><i class="fa-solid fa-location-dot"></i> Cambridge, MA 30245, USA</p>
-                                    <h6 class="fw-bolg">REAM MORE</h6>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col ">
-                            <div class="events-card border d-flex">
-                                <div class="col-3 d-flex justify-content-center pt-3">
-                                    <div class="square d-flex flex-column align-items-center justify-content-center">
-                                        <div class="">07</div>
-                                        <span>June 2022</span>
-                                    </div>
-                                </div>
-                                <div class="col-9 d-flex flex-column pt-3 align-items-start">
-                                    <h4>New York Coaching</h4>
-                                    <p><i class="fa-regular fa-clock"></i> 9:00 am - 5 p.m, Jan 7, 2022</p>
-                                    <p><i class="fa-solid fa-location-dot"></i> Cambridge, MA 30245, USA</p>
-                                    <h6 class="fw-bolg">REAM MORE</h6>
-                                </div>
-
-                            </div>
-                        </div>
-                        <div class="col ">
-                            <div class="events-card border d-flex">
-                                <div class="col-3 d-flex justify-content-center pt-3">
-                                    <div class="square d-flex flex-column align-items-center justify-content-center">
-                                        <div class="">07</div>
-                                        <span>June 2022</span>
-                                    </div>
-                                </div>
-                                <div class="col-9 d-flex flex-column pt-3 align-items-start">
-                                    <h4>New York Coaching</h4>
-                                    <p><i class="fa-regular fa-clock"></i> 9:00 am - 5 p.m, Jan 7, 2022</p>
-                                    <p><i class="fa-solid fa-location-dot"></i> Cambridge, MA 30245, USA</p>
-                                    <h6 class="fw-bolg">REAM MORE</h6>
+                                <div class="ps-3 right_event col-9 d-flex flex-column align-items-start">
+                                    <h6 class="fw-bold">New York Coaching</h6>
+                                    <p class="date m-0">
+                                        <i class="text_myprimary fa-regular fa-clock "></i>
+                                        9:00 am - 5 p.m, Jan 7, 2022
+                                    </p>
+                                    <p class="date "><i class="text_myprimary fa-solid fa-location-dot"></i> Cambridge,
+                                        MA 30245, USA</p>
+                                    <p class="fw-bold my_text_smaller">READ MORE</p>
                                 </div>
                             </div>
                         </div>
@@ -95,11 +67,15 @@ export default {
     position: absolute;
     top: 25%;
 
-    .square{
+    .square {
         width: 70px;
         height: 55px;
-        background-color: red;
         font-size: small;
     }
+    .date{
+        font-size: x-small;
+    }
 }
+
+
 </style>
